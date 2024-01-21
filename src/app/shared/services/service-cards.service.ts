04432dbb8d9f5034958bf6889af954f8ee8d954a
Your Name <daryaslinko@yahoo.com>
2024-01-21 15:08:35 +0300

    changed img again

--- src/app/shared/services/service-cards.service.ts
+++ src/app/shared/services/service-cards.service.ts
@@ -8,28 +8,28 @@ import {ServiceType} from "../../../types/service.type";
 export class ServiceCardsService {
   servicesCards: ServiceCardType[] = [
     {
-      image: '/assets/images/main/service-1.png',
+      image: './assets/images/main/service-1.png',
       name: 'Создание сайтов',
       text:'В краткие сроки мы создадим качественный и самое главное продающий сайт для продвижения Вашего бизнеса!',
       currency: '7 500',
       serviceType: ServiceType.createSite
     },
     {
-      image: '/assets/images/main/service-2.png',
+      image: './assets/images/main/service-2.png',
       name: 'Продвижение',
       text:'Вам нужен качественный SMM-специалист или грамотный таргетолог? Мы готовы оказать Вам услугу “Продвижения” на наивысшем уровне!',
       currency: '3 500',
       serviceType: ServiceType.promotion
     },
     {
-      image: '/assets/images/main/service-3.png',
+      image: './assets/images/main/service-3.png',
       name: 'Реклама',
       text:'Без рекламы не может обойтись ни один бизнес или специалист. Обращаясь к нам, мы гарантируем быстрый прирост клиентов за счёт правильно настроенной рекламы.',
       currency: '1 000',
       serviceType: ServiceType.marketing
     },
     {
-      image: '/assets/images/main/service-4.png',
+      image: './assets/images/main/service-4.png',
       name: 'Копирайтинг',
       text:'Наши копирайтеры готовы написать Вам любые продающие текста, которые не только обеспечат рост охватов, но и помогут выйти на новый уровень в продажах.',
       currency: '750',
--- src/app/shared/services/slides.service.ts
+++ src/app/shared/services/slides.service.ts
@@ -9,20 +9,20 @@ export class SlidesService {
 
   carouselSlides: CarouselSlideType[] = [
     {
-      backgroundUrl: '/assets/images/main/banner1.png',
+      backgroundUrl: './assets/images/main/banner1.png',
       name: 'Предложение месяца',
       title: 'Продвижение в Instagram для вашего бизнеса <span class="span">-15%</span>!',
       service: ServiceType.promotion
     },
     {
-      backgroundUrl: '/assets/images/main/banner2.png',
+      backgroundUrl: './assets/images/main/banner2.png',
       name: 'Акция',
       title: 'Нужен грамотный <span class="span">копирайтер</span>?',
       text: 'Весь декабрь у нас действует акция на работу копирайтера.',
       service: ServiceType.copyWriting
     },
     {
-      backgroundUrl: '/assets/images/main/banner3.png',
+      backgroundUrl: './assets/images/main/banner3.png',
       name: 'Новость дня',
       title: '<span class="span">6 место</span> в ТОП-10 <br> SMM-агенств Москвы!',
       text: 'Мы благодарим каждого, кто голосовал за нас!',
